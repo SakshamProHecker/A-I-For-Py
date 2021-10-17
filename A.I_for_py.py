37ad1555b4eee2eefb8f6ba627e162bc92f44fc4
SakshamProHecker <92621914+SakshamProHecker@users.noreply.github.com>
2021-10-17 10:13:53 +0545

    Add files via upload

---
+++ A.I_for_py.py
@@ -0,0 +1,50 @@
+
+# This is officially Created by Saksham Ishar 
+
+''' Author: Saksham
+Credit: Saksham '''
+
+
+from time import sleep
+
+
+
+# Let's create a simp Program right here! 
+
+print("What's your name? ")
+Me1 = input("You: ")
+print("Where do you live?")
+Me2 = input("You: ")
+print("What's your hobby? ")
+Me3 = input("You: ")
+
+PQ = ("Please Confirm your information ! ")
+
+
+
+print(PQ) ; sleep(2)
+
+
+
+
+F1 = ''' Name: ''',Me1
+
+F2 = 'Location: ',Me2
+
+F3 = 'Hobby: ',Me3
+
+print(F1) ; sleep(1)
+print(F2) ; sleep(1)
+print(F3) ; sleep(1)
+
+
+
+FH = (" Is this Right? ")
+
+print(FH) ; sleep(2)
+
+
+Saksham = input("You: ")
+
+
+print("Thanks ! I shall go now!")
